e42222ca99517f37418f2bd8ec6044f3bb6377f8
Geovanna Soto <158111914+Geovannayahweh@users.noreply.github.com>
2026-04-01 19:34:31 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,28 +1,25 @@
 CREATE DATABASE petlife;
 USE petlife;
+
 CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
+    num_telefone VARCHAR(15), 
     data_nascimento DATE NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
-    cep VARCHAR(10),
-    logradouro VARCHAR(150),
-    cidade VARCHAR(100),
-    estado VARCHAR(50),
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO usuario (nome, email, senha, data_nascimento, cpf, cep, logradouro, cidade, estado)
+
+INSERT INTO usuario (nome, email, senha, num_telefone, data_nascimento, cpf)
 VALUES (
     'Joao Abora',
     'abora@gmail.com',
     '123456',
+    '(41) 99999-9999',
     '2002-08-10',
-    '123.456.789-00',
-    '80000-000',
-    'Rua Teste',
-    'Curitiba',
-    'PR'
+    '123.456.789-00'
 );
+
 SELECT * FROM usuario;
