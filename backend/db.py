a59814018b4e6412660a146f0e89b8c8acd6e310
Thiago Christen <thiagochristen1209@gmail.com>
2026-04-14 21:43:57 -0300

    Foi alterado temporariamente os dados para acessar o BD, foi adicionado GET e POST referente a páginas que foram desenvolvidas/estão em desenvolvimento, foi criado a base.html para padronizar nossas telas, foi criado o diary.html mas apenas como modelo, dicas.html está em desenvolvimento com estrutura aplicada a FastAPI e conformidade com a main.py, mas com css em melhoria, foi criado o css das telas e todos pegam como padrão alguns comandos de estilização do global.css, foi adicionado imagens para testes de layout,foi adicionado a pasta telas, onde ficara nossos templates que servem de inspiração para futuros devs

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ import pymysql
 def get_db():
     return pymysql.connect(host="localhost",
         user="root",
-        password="root@",
+        password="122007",
         database="petlife",
         cursorclass=pymysql.cursors.DictCursor)
